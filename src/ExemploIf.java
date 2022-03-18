cac63bd560fa11c17464c946360356acdd7e744b
Bruna Stefany Oliveira de Barros <rm79210@estudante.fieb.edu.br>
2022-03-18 09:08:10 -0300

    Primeiro Commit

---
+++ src/ExemploIf.java
@@ -0,0 +1,22 @@
+import javax.swing.JOptionPane;
+
+public class ExemploIf {
+
+	public static void main(String[] args) {
+		
+		int x = 10;
+		int y = 5;
+		
+		if ( x > y ) {
+			JOptionPane.showMessageDialog(null, x + " é maior que " + y);
+		} else if ( x < y ) {
+			JOptionPane.showMessageDialog(null,
+					x + " é menor que " + y);
+		} else {
+			JOptionPane.showMessageDialog(null,
+					x + " é igual a " + y);
+		}
+
+	}
+
+}
